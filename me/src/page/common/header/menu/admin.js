70a760993c1e3eef07fc80f40b5663dff4b9c3a8
dtc03012 <dtc03012@naver.com>
2022-06-28 19:10:00 +0900

    Finish to create the admin

--- me/src/page/common/header/menu/admin.js
+++ me/src/page/common/header/menu/admin.js
@@ -3,8 +3,10 @@ import axios from "axios";
 import {v4} from 'uuid';
 import {Button, Fade, IconButton, Paper, Popper, Tooltip, Typography} from "@mui/material";
 import KeyIcon from "@mui/icons-material/Key";
+import LockIcon from '@mui/icons-material/Lock';
 import CustomInput from "../../../components/customInput";
 import Box from "@mui/material/Box";
+import {setCookie, getCookie} from '../../../../util/cookie'
 
 class Admin extends React.Component {
 
@@ -12,7 +14,6 @@ class Admin extends React.Component {
         super(props);
         // Don't call this.setState() here!
         this.state = {
-            isAdmin: this.CheckAdminLogin(),
             isOpen: false,
             anchorEl: null,
             loginID: "",
@@ -20,11 +21,14 @@ class Admin extends React.Component {
         };
         this.handleClick = this.handleClick.bind(this);
         this.handleChange = this.handleChange.bind(this);
-        this.Login = this.Login.bind(this);
-        this.CheckAdminLogin = this.CheckAdminLogin.bind(this);
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
+        this.checkAdminLogin = this.checkAdminLogin.bind(this);
+        this.isLoginFail = this.isLoginFail.bind(this);
+        this.adminIcon = this.adminIcon.bind(this);
     }
 
-    Login = (event) => {
+    login = (event) => {
         let uuid = v4()
         axios.post('/v2/login-admin', {
             password: this.state.loginID,
@@ -32,10 +36,11 @@ class Admin extends React.Component {
         }).then(
             response => {
                 if(response.data.isAdmin){
-                    localStorage.setItem('uuid', uuid)
+                    setCookie('uuid', uuid)
                     this.setState({
+                        isAdmin: true,
                         isOpen: false,
-                        loginId : "",
+                        loginID: "",
                     })
                 }else {
                     this.setState({
@@ -46,20 +51,38 @@ class Admin extends React.Component {
         )
     }
 
-    CheckAdminLogin = (event) =>  {
-        let uuid = localStorage.getItem("uuid")
+    logout = (event) => {
+        setCookie('uuid','')
+        this.setState({
+            isAdmin: false,
+            isOpen: false,
+        })
+    }
+
+    checkAdminLogin = (event) =>  {
+        let uuid = getCookie('uuid')
         if(uuid) {
+            let isCheck = false
             axios.post('/v2/find-admin-uuid', {
                 uuid: uuid
             }).then(
                 response => {
                     if(response.data.isFind) {
-                        return true
+                        this.setState({
+                            isAdmin: true
+                        })
+                    } else {
+                        this.setState({
+                            isAdmin: false
+                        })
                     }
                 }
             )
+            return
         }
-        return false
+        this.setState({
+            isAdmin: false
+        })
     }
 
     handleClick = (event) => {
@@ -77,21 +100,47 @@ class Admin extends React.Component {
         })
     }
 
-    render() {
-        this.canBeOpen = this.state.isOpen && Boolean(this.state.anchorEl);
-        this.id = this.canBeOpen ? 'transition-popper' : undefined;
+    componentDidMount() {
+        this.checkAdminLogin()
+    }
 
+    isLoginFail() {
+        if(this.state.loginFail) {
+            return (
+                <Typography sx={{
+                    fontFamily: 'Cinzel',
+                    fontSize: 12,
+                    fontWeight: 900,
+                    color: '#f44336',
+                    p: 2,
+                }}>
+                    Wrong!
+                </Typography>
+            )
+        }
+        return undefined
+    }
+
+    adminIcon() {
+        if(this.state.isAdmin) {
+            return (
+                <LockIcon sx={{p: 2}}/>
+            )
+        }
+        return (
+            <KeyIcon sx={{p: 2}}/>
+        )
+    }
+    render() {
         return (
             <div>
                 <IconButton aria-describedby={this.id} type="button" onClick={this.handleClick}>
-                    <Tooltip title="Admin">
-                        <KeyIcon sx={{ p : 2 }} />
-                    </Tooltip>
+                    {this.adminIcon()}
                 </IconButton>
-                <Popper id={this.id} open={this.state.isOpen} anchorEl={this.state.anchorEl} transition>
-                    {({ TransitionProps }) => (
+                <Popper open={this.state.isOpen && !this.state.isAdmin} anchorEl={this.state.anchorEl} transition>
+                    {({TransitionProps}) => (
                         <Fade {...TransitionProps} timeout={350}>
-                            <Paper sx={{ border: 1, p: 1}}>
+                            <Paper sx={{border: 1, p: 1}}>
                                 <Box sx={{
                                     paddingLeft: 1,
                                     paddingRight: 1,
@@ -106,12 +155,39 @@ class Admin extends React.Component {
                                         type="text"
                                     />
                                 </Box>
-                                <Button type="button"  sx={{
+                                <Box sx={{
+                                    display: "flex",
+                                    justifyContent: 'space-between'}}
+                                >
+                                    <Button type="button" sx={{
+                                        fontFamily: 'Cinzel',
+                                        fontWeight: 900,
+                                        color: '#212121',
+                                    }} onClick={this.login}>
+                                        Login
+                                    </Button>
+                                    {this.isLoginFail()}
+                                </Box>
+                            </Paper>
+                        </Fade>
+                    )}
+                </Popper>
+                <Popper id={this.id} open={this.state.isOpen && this.state.isAdmin} anchorEl={this.state.anchorEl}
+                        transition>
+                    {({TransitionProps}) => (
+                        <Fade {...TransitionProps} timeout={350}>
+                            <Paper sx={{border: 1, p: 1}}>
+                                <Button type="button" sx={{
                                     fontFamily: 'Cinzel',
                                     fontWeight: 900,
-                                    color: '#212121'
-                                }} onClick={this.Login}>
-                                    Login
+                                    color: '#ffffff',
+                                    bgcolor: '#f44336',
+                                    '&:hover': {
+                                        bgcolor: '#000000',
+                                        color: 'white',
+                                    },
+                                }} onClick={this.logout}>
+                                    Logout
                                 </Button>
                             </Paper>
                         </Fade>
